4d074cf7d5e73a34686028361de03434ca104aab
Devika P S <devu1011depu@gmail.com>
2024-12-27 12:24:11 +0530

    back to normal app.js file

--- Backend/app.js
+++ Backend/app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
 const cors = require("cors");
-const path = require("path");
 const ENV = require("./Config/ENV");
 const connectDb = require("./Config/Connection");
 const adminRoute = require("./Routes/adminRoutes");
@@ -10,6 +9,7 @@ const userRoute = require("./Routes/userRoutes");
 const voucherRoute = require("./Routes/voucherRoutes");
 const bidRoute = require("./Routes/bidRoutes");
 const { initSocket } = require("./socket/socketInstance");
+const path = require("path");
 
 const http = require("http");
 
@@ -21,7 +21,6 @@ const server = http.createServer(app);
 // Initialize Socket.IO
 initSocket(server);
 
-// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
@@ -31,29 +30,7 @@ app.use(
   })
 );
 
-// API Routes
-app.use("/api/admin", adminRoute);
-app.use("/api/user", userRoute);
-app.use("/api/voucher", voucherRoute);
-app.use("/api/bid", bidRoute);
 
-// Serve static files from the frontend build directory
-app.use(express.static(path.join(__dirname)));
-
-// Serve index.html for the root route
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname,  'index.html'));
-});
-
-// Handle other routes for SPA
-app.get('*', (req, res, next) => {
-  // If the request is for an API route, pass it to the next middleware
-  if (req.url.startsWith('/api/')) {
-    return next();
-  }
-  // Otherwise serve the frontend app
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
 
 // Global error handler
 app.use((err, req, res, next) => {
@@ -63,6 +40,16 @@ app.use((err, req, res, next) => {
     error: process.env.NODE_ENV === 'development' ? err.message : {}
   });
 });
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,  'index.html'));
+});
+
+app.use("/api/admin",adminRoute);
+app.use("/api/user",userRoute);
+app.use("/api/voucher",voucherRoute);
+app.use("/api/bid",bidRoute);
+
 
 const port = ENV.PORT || 5002;
 
