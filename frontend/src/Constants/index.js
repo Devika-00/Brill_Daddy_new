e80fdae1c3a62f1c7b31751b069f15f1f4ba18a8
apple <apple@apples-MacBook-Air.local>
2024-12-28 14:47:28 +0530

    QA test 0.0.34

--- frontend/src/Constants/index.js
+++ frontend/src/Constants/index.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 export const SERVER_URL = import.meta.env.VITE_API_URL;
 export const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
+export const CLOUDINARY_UPLOAD_API = "https://api.cloudinary.com/v1_1/dbl7wgz51/upload";
+export const cloudinaryUploadPreset = "ml_default";
 
 // Create axios instance with custom config
 const axiosInstance = axios.create({
