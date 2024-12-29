a6d4f94a7c54e425e6ce0585db65d2ddb4433e1c
apple <apple@apples-MacBook-Air.local>
2024-12-29 21:40:30 +0530

    QA test 0.0.54

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,30 +1,31 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-export default defineConfig({
-  plugins: [react()],
-  preview: {
-    port: 4173,
-    strictPort: true,
-  },
-  server: {
-    port: 5173,
-    strictPort: true,
-    proxy: {
-      '/api': {
-        target: import.meta.env.VITE_API_URL,
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '')
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [react()],
+    preview: {
+      port: 4173,
+      strictPort: true,
+    },
+    server: {
+      port: 5173,
+      strictPort: true,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL || 'http://localhost:5002',
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
       }
+    },
+    define: {
+      'process.env': {}
     }
-  },
-  define: {
-    'process.env': {}
   }
 })
